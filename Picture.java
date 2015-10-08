089cc83d8391f06c79343d8dd3a8313efcbaba12
liveworldstrike <lazodellamas12@gmail.com>
2015-10-08 12:41:42 +0200

    sol naciente

--- Picture.java
+++ Picture.java
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle sun2;
 
     /**
      * Constructor for objects of class Picture
@@ -49,11 +50,18 @@ public class Picture
         roof.makeVisible();
 
         sun = new Circle();
-        sun.changeColor("yellow");
+        sun.changeColor("blue");
         sun.moveHorizontal(100);
         sun.moveVertical(-40);
         sun.changeSize(80);
         sun.makeVisible();
+        
+        sun2 = new Circle();
+        sun2.changeColor("green");
+        sun2.moveHorizontal(-200);
+        sun2.moveVertical(-40);
+        sun2.changeSize(80);
+        sun2.makeVisible();
     }
 
     /**
